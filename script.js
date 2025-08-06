b6b32ad848d3d80f6173c10a9b0af6955b82c549
Animatedghost <animatedghost01@gmail.com>
2025-08-06 01:24:44 +0100

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const messages = [
   "Hey my sweet cheeks…<br>I made this just for you 😚 Keep clicking — I’ve got a few things to say.",
-  "You’ve worked so hard for this moment.<br>I’m so proud of the strong, smart, beautiful woman you are 💜📚",
+  "You’re worked so hard for this moment.<br>I’m so proud of the strong, smart, beautiful woman you are 💜📚",
   "Don’t stress baby.<br>You’ve got this exam in the bag — and I’ve got your back always 🥺💪🏽",
   "Even when you’re nervous, remember this:<br>You’re magic, you’re brilliant, and you’re never alone ✨💞",
-  "After this exam?<br>It’s you + me + celebration mode 🥳🍦 (and maybe some cuddles too)",
-  "One last thing before you go ace it…<br>I believe in you with everything in me, and I love you endlessly ❤️🧸"
+  "After this exam?<br>It’s you + me + celebration mode 🥳🍦 (and maybe some cuddles too lol)",
+  "Just keep doing your best love and you are going to ace it…<br>I believe in you with everything in me, and I love you endlessly ❤️🧸"
 ];
 
 const teddies = [
-  "teddy1.png",
-  "teddy2.png",
-  "teddy3.png",
-  "teddy4.png",
-  "teddy5.png",
-  "teddy6.png"
+  "teddy1.jpg",
+  "teddy2.jpg",
+  "teddy3.jpg",
+  "teddy4.jpg",
+  "teddy5.jpg",
+  "teddy6.jpg"
 ];
 
 let current = 0;
