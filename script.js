42cadffd828863ea3f3a3b7d196a4bff1b32cb96
Animatedghost <animatedghost01@gmail.com>
2025-08-06 01:28:50 +0100

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const messages = [
   "Hey my sweet cheeks…<br>I made this just for you 😚 Keep clicking — I’ve got a few things to say.",
   "You’re working so hard for this moment.<br>I’m so proud of the strong, smart, beautiful woman you are 💜📚",
-  "Don’t stress baby.<br>You’ve got this exam in the bag — and I’ve got your back always 🥺💪🏽",
+  "Don’t stress baby.<br>You’ve got this physiology in the bag — and know I’ve got your back always 🥺💪🏽",
   "Even when you’re nervous, remember this:<br>You’re magic, you’re brilliant, and you’re never alone ✨💞",
   "After this exam?<br>It’s you + me + celebration mode 🥳🍦 (and maybe some cuddles too lol)",
   "Just keep doing your best love and you are going to ace it…<br>I believe in you with everything in me, and I love you endlessly ❤️🧸"
